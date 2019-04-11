b834f3653f02684faa9e7b2e0405c049b666c600
BelalElkady <belal.elkady4@gmail.com>
2019-04-11 16:58:14 +0200

    Single Page Applications Part 1

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,9 +1,40 @@
 import React from "react";
+import Card from "reactstrap/lib/Card";
+import CardImg from "reactstrap/lib/CardImg";
+import CardBody from "reactstrap/lib/CardBody";
+import CardTitle from "reactstrap/lib/CardTitle";
+import CardSubtitle from "reactstrap/lib/CardSubtitle";
+import CardText from "reactstrap/lib/CardText";
 
-const Home = () => {
+const RenderCard = ({ item }) => {
+  return (
+    <Card>
+      <CardImg src={item.image} alt={item.name} />
+      <CardBody>
+        <CardTitle> {item.name} </CardTitle>
+        {item.designation ? (
+          <CardSubtitle>{item.designation}</CardSubtitle>
+        ) : null}
+        <CardText>{item.description}</CardText>
+      </CardBody>
+    </Card>
+  );
+};
+
+const Home = props => {
   return (
     <div className="container">
-      <h4>Home</h4>
+      <div className="row">
+        <div className="col-12 col-md m-1">
+          <RenderCard item={props.dish} />
+        </div>
+        <div className="col-12 col-md m-1">
+          <RenderCard item={props.promotion} />
+        </div>
+        <div className="col-12 col-md m-1">
+          <RenderCard item={props.leader} />
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,15 +4,22 @@ import { Navbar, NavbarBrand } from "reactstrap";
 import Menu from "./MenuComponent";
 import DishDetail from "./DishdetailComponent";
 import { DISHES } from "../shared/dishes";
+import { COMMENTS } from "../shared/comments";
+import { PROMOTIONS } from "../shared/promotions";
+import { LEADERS } from "../shared/leaders";
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
 import Home from "./HomeComponent";
+import Contact from "./ContactComponent";
 import { Switch, Route, Redirect } from "react-router-dom";
 class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      dishes: DISHES
+      dishes: DISHES,
+      promotions: PROMOTIONS,
+      comments: COMMENTS,
+      leaders: LEADERS
     };
   }
 
@@ -21,11 +28,25 @@ class Main extends Component {
       <div>
         <Header />
         <Switch>
-          <Route path="/home" component={Home} />
+          <Route
+            path="/home"
+            component={() => (
+              <Home
+                dish={this.state.dishes.filter(dish => dish.featured)[0]}
+                promotion={
+                  this.state.promotions.filter(
+                    promotion => promotion.featured
+                  )[0]
+                }
+                leader={this.state.leaders.filter(leader => leader.featured)[0]}
+              />
+            )}
+          />
           <Route
             path="/menu"
             component={() => <Menu dishes={this.state.dishes} />}
           />
+          <Route path="/contactus" component={Contact} />
           <Redirect to="/home" />
         </Switch>
 
